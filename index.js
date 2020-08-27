7ecaf196ed97b52680ed57dcf6087c6810b14aec
John Ratanaprasatporn <jratana1@gmail.com>
2020-08-27 04:01:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(line){
   else{
     var result = ('The line is currently:')
     for (let i=0; i < line.length; ++i){
-    result = result+` ${i+1}. ${line[i]}`
+    result = result+` ${i+1}. ${line[i],}`
     }
     return(result)
   }
